c178689d49dd9bce856956d47580bb5f2ed46544
MonicaHillman <mmhillman1@ucs.br>
2022-10-27 12:08:44 -0300

    testando caminhos publicos

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,10 +1,11 @@
 import styles from './Banner.module.css';
 
 function Banner({ imagem }) {
+
     return (
         <div
             className={styles.capa}
-            style={{ backgroundImage: `url('%PUBLIC_URL%/assets/Banner/${imagem}.png')` }}>
+            style={{ backgroundImage: `url('/assets/Banner/${imagem}.png')` }}>
         </div>
     )
 }
