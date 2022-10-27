5126c0751cf27f9df2087b8f7b8b2fe6823d08e1
MonicaHillman <mmhillman1@ucs.br>
2022-10-27 12:59:56 -0300

    ajuste no nome da pasta

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -5,7 +5,7 @@ function Banner({ imagem }) {
     return (
         <div
             className={styles.capa}
-            style={{ backgroundImage: `url('/assets/Banner/${imagem}.png')` }}>
+            style={{ backgroundImage: `url('/assets/banner/${imagem}.png')` }}>
         </div>
     )
 }
